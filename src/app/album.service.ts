a32bafd21ac3338fa327152af0fa1ec7baa44f8d
SkyeN <skye@dames.es>
2018-10-30 13:12:05 -0700

    finding and retrieving entries from firebase

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -14,11 +14,7 @@ export class AlbumService {
   addAlbum(newAlbum: Album) {
     this.albums.push(newAlbum);
   }
-  // getAlbumById(albumId: number){
-  //   for (var i = 0; i <= ALBUMS.length - 1; i++) {
-  //     if (ALBUMS[i].id === albumId) {
-  //       return ALBUMS[i];
-  //     }
-  //   }
-  // }
+  getAlbumById(albumId: string){
+    return this.database.object('albums/' + albumId);
+  }
 }
